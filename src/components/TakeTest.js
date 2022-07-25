3f1d0946aa5997cacc9f84b001af8c3d3c669403
Kartik Tyagi <32240906+genialkartik@users.noreply.github.com>
2022-07-25 21:05:07 +0530

    ext html

--- src/components/TakeTest.js
+++ src/components/TakeTest.js
@@ -1,15 +1,16 @@
 import React from "react";
-import TestFile from "../test/story.html";
-
-const Iframe = ({ src, height, width }) => {
-  return (
-    <div>
-      <iframe src={src} height={height} width={width} title="iframe" />
-    </div>
-  );
-};
+// var page = require("../test/story.html");
+import page from "../test/story.html";
+var htmlDoc = { __html: page };
 
 export default function TakeTest() {
-  return <Iframe src={TestFile} height="720" width="80%" />;
-  // return <Iframe src="http://plnkr.co/" height="720" width="80%" />;
+  return (
+    <div dangerouslySetInnerHTML={htmlDoc} />
+    // <iframe src={page}></iframe>
+    // <iframe
+    //   src={"https://psycho-test.netlify.app/story_html5.html?lms=1"}
+    //   height="720"
+    //   width="80%"
+    // />
+  );
 }
